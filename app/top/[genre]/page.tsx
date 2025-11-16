b8b517590c109515054f7c031ab3e44e5e532f6f
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-11-16 22:08:30 +0200

    fetch and display trending movies

--- app/top/[genre]/page.tsx
+++ app/top/[genre]/page.tsx
@@ -1,7 +1,30 @@
+import Results from "@/components/Results"
+
  
-function TopPages() {
+  const API_KEY = process.env.API_KEY
+
+async function TopPages({params}: {params :{genre : string}}) {
+
+  const {genre} = await params
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'rated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  )
+
+  if(!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = await res.json()
+
+  const results=data.results
+
   return (
-    <div>TopPages</div>
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
 
