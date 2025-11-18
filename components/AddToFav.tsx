729febae488d86047291544ec4137f9591d5061b
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-11-18 20:43:06 +0200

    function AddToFav component

--- components/AddToFav.tsx
+++ components/AddToFav.tsx
@@ -1,3 +1,8 @@
+'use client'
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 type AddToFavProps = {
   movieId: string;
   title: string;
@@ -15,7 +20,72 @@ function AddToFav({
   releaseDate,
   voteCount,
 }: AddToFavProps) {
-  return <div>AddToFav</div>;
+
+   const [isFav, setIsFav] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { isSignedIn, user, isLoaded } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (isLoaded && isSignedIn && user) {
+      setIsFav(user.publicMetadata?.favs?.includes(movieId));
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  }, [movieId, isLoaded, isSignedIn, user]);
+
+  const handleFavClick = async () => {
+    setIsLoading(true);
+    if (!isSignedIn) {
+      setIsLoading(false);
+      router.push('/sign-in');
+      return;
+    }
+    try {
+      const res = await fetch('/api/user/fav', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          movieId,
+          title,
+          overview,
+          releaseDate,
+          voteCount,
+          image,
+        }),
+      });
+
+      if (res.ok) {
+        setIsFav(!isFav);
+      } else {
+        console.error('Failed to update favorites');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+     <div>
+      <button
+        onClick={handleFavClick}
+        className={`p-2 rounded cursor-pointer  ${isFav ? 'bg-red-300 dark:bg-red-600' : 'bg-gray-300 dark:bg-gray-600'}`}
+        disabled={isLoading}
+      >
+        {isLoading
+          ? 'Loading...'
+          : isFav
+          ? 'Remove from Favorites'
+          : 'Add to Favorites'}
+      </button>
+    </div>
+  );
 }
 
 export default AddToFav;
